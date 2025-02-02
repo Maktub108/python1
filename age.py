ad702880f8aa755d8d82a6bb8b2ab00d1f20c9d7
Maktub108 <e.kozulin@yandex.ru>
2025-02-02 21:13:25 +0500

    Add file

---
+++ age.py
@@ -0,0 +1,8 @@
+name = input("Введите ваше имя:")
+age = int(input("Введите ваш возраст в годах):"))
+months = age * 12
+days = age * 365
+hours = days * 24
+print(f"Привет {name}! Тебе {age} лет.")
+print(f"Ты прожил(а) {months} месяцев, {days} дней и {hours} часов.")
+
