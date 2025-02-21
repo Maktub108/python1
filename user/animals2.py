327a88417e02189b96152b101c1cde3e25fcc462
Maktub108 <e.kozulin@yandex.ru>
2025-02-21 21:47:26 +0500

    Add file

--- user/animals2.py
+++ user/animals2.py
@@ -1,5 +1,6 @@
 # Создаём базовый класс `Animal`, который будет содержать общие атрибуты (например, `name`, `age`) и методы (`make_sound()`, `eat()`) для всех животных.
 # Реализуем наследование, создав подклассы `Bird`, `Mammal`, и `Reptile`, которые наследуют от класса `Animal`
+
 class Animal():
 
     def __init__(self, name, age):
@@ -10,25 +11,23 @@ class Animal():
         print(f"{self.name} ест.")
 
 
- class Bird(Animal):
-
-    def __init__(self, age, wing_span):
+class Bird(Animal):
+    def __init__(self, name, age, wing_span):
         super().__init__(name, age)
         self.wing_span = wing_span
 
     def make_sound(self):
         return "Чирик!"
 
- class Mammal(Animal):
-
-    def __init__(self, name,age, habitat):
+class Mammal(Animal):
+    def __init__(self, name, age, habitat):
          super().__init__(name, age)
          self.habitat = habitat
 
     def make_sound(self):
         return "Рррр!"
 
- class Reptile(Animal):
+class Reptile(Animal):
 
      def __init__(self, name, age, scale_type):
          super().__init__(name, age)
@@ -37,18 +36,18 @@ class Animal():
      def make_sound(self):
          return "Шшш!"
 
-def animal_sound(animals):
-    for animal in animals:
+     def animal_sound(animals):
+      for animal in animals:
         print(f"{animal.name} говорит: {animal.make_sound()}")
 
- class ZooKeeper:
+class ZooKeeper:
     def __init__(self, name):
         self.name = name
 
     def feed_animal(self, animal):
         print(f"{self.name} кормит {animal.name}.")
 
- class Veterinarian:
+class Veterinarian:
     def __init__(self, name):
         self.name = name
 
@@ -56,7 +55,7 @@ def animal_sound(animals):
         print(f"{self.name} лечит {animal.name}.")
 
 
- class Zoo:
+class Zoo:
      def __init__(self):
         self.animals = []
         self.staff = []
@@ -69,20 +68,16 @@ def animal_sound(animals):
         self.staff.append(staff_member)
         print(f"Сотрудник {staff_member.name} добавлен в зоопарк.")
 
+if __name__ == "__main__":
 
-
-    if __name__ == "__main__":
-
-    parrot = Bird(name="Попугай", age=10,wing_span=15)
+    parrot = Bird(name="Попугай", age=4, wing_span=15)
     lion = Mammal(name="Лев", age=5, habitat="Савана")
     snake = Reptile(name="Змея", age=3, scale_type="Чешуя")
-
 
     zoo = Zoo()
     zoo.add_animal(parrot)
     zoo.add_animal(lion)
     zoo.add_animal(snake)
-
 
     zookeeper = ZooKeeper(name="Александр")
     veterinarian = Veterinarian(name="Николай")
@@ -90,17 +85,15 @@ def animal_sound(animals):
     zoo.add_staff(zookeeper)
     zoo.add_staff(veterinarian)
 
-    animal_sound(zoo.animals)
+    animal_sound = zoo.animals
 
     zookeeper.feed_animal(lion)
     veterinarian.heal_animal(snake)
 
 
-    zoo.save_to_file('zoo_data.json')
 
-    new_zoo = Zoo()
-    new_zoo.load_from_file('zoo_data.json')
 
-    animal_sound(new_zoo.animals)
+
+
 
 
